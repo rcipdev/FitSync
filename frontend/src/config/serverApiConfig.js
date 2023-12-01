b8f80789fd9bd85572cf2c43c5264dcb06957931
Ruchik Pravasi <ruchikpravasi@Ruchiks-MBP-2.attlocal.net>
2023-11-30 18:14:39 -0800

    bugfix

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,13 +1,13 @@
 export const API_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
     ? import.meta.env.VITE_BACKEND_SERVER + 'api/'
-    : 'http://localhost:8888/api/';
+    : 'http://13.127.36.67:8888/api/';
 export const BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER
-    : 'http://localhost:8888/';
+    : 'http://13.127.36.67:8888/';
 export const DOWNLOAD_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER + 'download/'
-    : 'http://localhost:8888/download/';
+    : 'http://13.127.36.67:8888/download/';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
