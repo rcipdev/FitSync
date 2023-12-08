16d094537dea7bd9e92792a2dad8dee73cd690b0
Ruchik Pravasi <ruchikpravasi@Ruchiks-MBP-2.attlocal.net>
2023-12-07 16:21:55 -0800

    bugfix

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,13 +1,13 @@
 export const API_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
     ? import.meta.env.VITE_BACKEND_SERVER + 'api/'
-    : 'http://13.127.36.67:8888/api/';
+    : 'http://13.235.241.179/:8888/api/';
 export const BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER
-    : 'http://13.127.36.67:8888/';
+    : 'http://13.235.241.179/:8888/';
 export const DOWNLOAD_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER + 'download/'
-    : 'http://13.127.36.67:8888/download/';
+    : 'http://13.235.241.179/:8888/download/';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default ({ mode }) => {
   const proxy_url =
     process.env.VITE_DEV_REMOTE === 'remote'
       ? process.env.VITE_BACKEND_SERVER
-      : 'http://13.127.36.67:8888/';
+      : 'http://13.235.241.179/:8888/';
 
   const config = {
     plugins: [react()],
