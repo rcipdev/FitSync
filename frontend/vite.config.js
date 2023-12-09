f14b647892dfac151a01ccabbf67a992e85a1e81
Ruchik Pravasi <ruchikpravasi@Ruchiks-MBP-2.attlocal.net>
2023-12-09 04:09:59 -0800

    end url change in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default ({ mode }) => {
   const proxy_url =
     process.env.VITE_DEV_REMOTE === 'remote'
       ? process.env.VITE_BACKEND_SERVER
-      : 'http://localhost:8888/';
+      : 'http://65.0.182.153:8888/';
 
   const config = {
     plugins: [react()],
