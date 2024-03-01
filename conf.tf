7ea6983d22fc5d17285a79e82e893b03688cade2
rcipdev <ruchikpravasi@gmail.com>
2024-03-01 03:16:38 -0800

    terraform file added

--- conf.tf
+++ conf.tf
@@ -2,8 +2,8 @@ provider "google" {
   region = "us-west4-b"
 }
 
-resource "gcp_instance" "example" {
-  ami           = "460526582968571464"
+resource "google_compute_instance" "instance2402" {
+  id           = "460526582968571464"
   instance_type = "e2-standard-16"
 }
 output "public_ip" {
